77faec799d9a99ec2d3be5f5c1a6df6b00bcc332
melprog <cantoumelanie@gmail.com>
2024-09-01 18:22:51 -0300

    first commit

--- eventos.js
+++ eventos.js
@@ -1,3 +1,9 @@
-document.getElementById("btn").addEventListener("click", function() {
+// eventos.js
+document.getElementById("btn").addEventListener("click", function(event) {
+    event.stopPropagation(); 
+    alert('Hola!');
+});
+
+document.getElementById("div").addEventListener("click", function() {
     alert("Hola! Soy el div");
 });
